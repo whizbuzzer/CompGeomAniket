311ef510cbf4832294520c935e400602fbc6f927
Aniket Prabhu <58381051+whizbuzzer@users.noreply.github.com>
2022-10-31 16:07:45 -0700

    Delete tests/build directory

--- tests/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/whizbuzzer/Desktop/CPP_projects/CompGeomAniket/tests
-# Build directory: /home/whizbuzzer/Desktop/CPP_projects/CompGeomAniket/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/whizbuzzer/Desktop/CPP_projects/CompGeomAniket/tests/build/cga_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
